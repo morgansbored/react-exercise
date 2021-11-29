84c57d9f815867b2fb302fba3571d8c2841eafc4
morgansbored <morgan@morganpearse.com>
2021-11-29 14:52:01 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import React, {useState} from 'react';
 import styled, { ThemeProvider } from "styled-components";
 import * as yup from "yup";
 import { Field, Form, Formik } from "formik";
-import { Button, TextField, crukTheme } from "@cruk/cruk-react-components";
+import { Button, Text, TextField, Select, crukTheme } from "@cruk/cruk-react-components";
 
 const SiteWrapper = styled.div`
   max-width: 1200px;
@@ -9,28 +10,40 @@ const SiteWrapper = styled.div`
   padding: 1rem;
 `;
 
-function App() {
+function App({ items }) {
+  const [searchResults, setSearchResults] = useState(items);
+
   const formSchema = yup.object().shape({
-    town: yup.string()
-      .required("Please enter keywords to search.")
+    keywords: yup.string()
+      .required("Please enter keywords to search."),
+    mediaType: yup.string()
+      .required("Please choose a media type."),
+    yearStart: yup.string()
+      .required("Please enter a year."),
   });
 
   return (
     <ThemeProvider theme={crukTheme}>
       <SiteWrapper>
         <div>
-          <h1>CRUK technical exercise - React</h1>
+          <h1>NASA Image Library Search</h1>
         </div>
         <div>
           <Formik
-            validateOnChange
             initialValues={{
               keywords: "",
+              mediaType: "",
+              yearStart: "",
             }}
             validationSchema={formSchema}
-            onSubmit={(values) => {
-              console.log(values);
-            }}>
+            onSubmit={async (values) => {
+              const results = await fetch(
+                `https://images-api.nasa.gov/search?media_type=${values.mediaType}&q=${values.keywords}&year_start=${values.yearStart}`
+              );
+              const previews = await results.json();
+              setSearchResults(await previews.collection.items);
+            }}
+            >
             {({ errors, touched }) => {
               return (
                 <Form>
@@ -47,6 +60,45 @@ function App() {
                       </>
                     )}
                   </Field>
+                  <Field name="mediaType">
+                  {({ field }) => (
+                      <>
+                        <Select
+                          label="Media type"
+                          required
+                          {...field}
+                        >
+                          <option value=""
+                          >
+                            --Please choose an option--
+                          </option>
+                          <option value="audio">
+                            Audio
+                          </option>
+                          <option value="video">
+                            Video
+                          </option>
+                          <option value="image">
+                            Image
+                          </option>
+                        </Select>
+                        {errors.mediaType && touched.mediaType && <p>{errors.mediaType}</p>}
+                      </>
+                    )}
+                  </Field>
+                  <Field name="yearStart">
+                    {({ field }) => (
+                      <>
+                        <TextField
+                          label="Year start" 
+                          type="text"
+                          required
+                          {...field}
+                        />
+                        {errors.yearStart && touched.yearStart && <p>{errors.yearStart}</p>}
+                      </>
+                    )}
+                  </Field>
 
                   <Button type="submit">Submit</Button>
                 </Form>
@@ -54,9 +106,21 @@ function App() {
             }}
           </Formik>
         </div>
+        <div>
+        {searchResults &&
+              searchResults.map((preview) => (
+                <Text
+                  key={preview.data[0].nasa_id}
+                  nasaId={preview.data[0].nasa_id}
+                >
+                {preview.data[0].description}
+                </Text>
+              ))}
+        </div>
       </SiteWrapper>
     </ThemeProvider>
   );
 }
 
 export default App;
+
